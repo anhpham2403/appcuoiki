31e4c6aa2638b97ad270e87b3e2d27710357ab15
anhpham2403 <phamtheanh243@gmail.com>
2017-12-07 09:22:20 +0700

    fix

--- src/main/java/com/anh/movie/entities/Character.java
+++ src/main/java/com/anh/movie/entities/Character.java
@@ -2,6 +2,7 @@ package com.anh.movie.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import javax.persistence.EmbeddedId;
@@ -17,18 +18,17 @@ import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 @Entity
+@Table(name = "charac", catalog = "movie")
+public class Character implements Serializable {
 
-@Table(name = "character", catalog = "movie", uniqueConstraints = { @UniqueConstraint(columnNames = { "id_movie" }),
-		@UniqueConstraint(columnNames = { "id_actor" }) })
-public class Character implements Serializable{
-	private int idCharacter;
+	private pk id;
 	private Actor actor;
 	private Movie movie;
 	private String character;
 	private String profilePath;
-	
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "id_actor", nullable = false)
+
+	@ManyToOne(cascade=CascadeType.ALL,fetch = FetchType.EAGER)
+	@JoinColumn(name = "id_actor", nullable = false,insertable=false, updatable=false)
 	public Actor getActor() {
 		return actor;
 	}
@@ -37,8 +37,8 @@ public class Character implements Serializable{
 		this.actor = actor;
 	}
 
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "id_movie", nullable = false)
+	@ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinColumn(name = "id_movie", nullable = false,insertable=false, updatable=false)
 	public Movie getMovie() {
 		return movie;
 	}
@@ -64,15 +64,47 @@ public class Character implements Serializable{
 	public void setProfilePath(String profilePath) {
 		this.profilePath = profilePath;
 	}
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id_character", unique = true, nullable = false)
-	public int getIdCharacter() {
-		return idCharacter;
+
+	@EmbeddedId
+	public pk getId() {
+		return id;
 	}
 
-	public void setIdCharacter(int idCharacter) {
-		this.idCharacter = idCharacter;
+	public void setId(pk id) {
+		this.id = id;
 	}
-	
+
+	@Embeddable
+	public static class pk implements Serializable {
+
+		private int idActor;
+		private int idMovie;
+
+		public pk() {
+		}
+		
+		public pk(int idActor, int idMovie) {
+			this.idActor = idActor;
+			this.idMovie = idMovie;
+		}
+
+		@Column(name = "id_actor")
+		public int getIdActor() {
+			return idActor;
+		}
+
+		public void setIdActor(int idActor) {
+			this.idActor = idActor;
+		}
+
+		@Column(name = "id_movie")
+		public int getIdMovie() {
+			return idMovie;
+		}
+
+		public void setIdMovie(int idMovie) {
+			this.idMovie = idMovie;
+		}
+
+	}
 }
