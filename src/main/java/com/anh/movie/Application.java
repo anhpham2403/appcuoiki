a685ee8a678dc55045df56835471bf957882c04f
anhpham2403 <phamtheanh243@gmail.com>
2018-10-02 21:03:23 +0700

    add firebase

--- src/main/java/com/anh/movie/Application.java
+++ src/main/java/com/anh/movie/Application.java
@@ -9,7 +9,17 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.ServerProperties;
 import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import javax.ws.rs.ApplicationPath;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.security.Key;
 import java.util.Date;
 
@@ -17,6 +27,7 @@ import java.util.Date;
 public class Application extends ResourceConfig { 
 
 	private static Key key;
+	private static DatabaseReference  mDatabase;
 
 	public Application() {
 		this(MacProvider.generateKey());
@@ -38,6 +49,7 @@ public class Application extends ResourceConfig {
         });
 		property("jersey.config.beanValidation.enableOutputValidationErrorEntity.server", "true");
 		property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true);
+		syncData();
 
 	}
 
@@ -49,4 +61,25 @@ public class Application extends ResourceConfig {
 		Application.key = key;
 	}
 
+	public static void syncData() {
+		FileInputStream serviceAccount = null;
+		try {
+			serviceAccount = new FileInputStream("currencyserver240395-bd7933f24eae.json");
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		FirebaseOptions options = null;
+		try {
+			options = new FirebaseOptions.Builder().setCredentials(GoogleCredentials.fromStream(serviceAccount))
+					.setDatabaseUrl("https://currencyserver240395.firebaseio.com/").build();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		FirebaseApp.initializeApp(options);
+		mDatabase = FirebaseDatabase.getInstance().getReference("currency");
+		mDatabase.setValueAsync("©2016 androidhive. All rights Reserved");
+
+	}
 }
